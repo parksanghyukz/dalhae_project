da2a42b6093f8f6d3ddb5a40cf4024d6a64a0af1
thiago <pash609@naver.com>
2023-08-18 17:59:00 +0900

    waiting_img_update

--- src/pages/MoreCustomerCenter.js
+++ src/pages/MoreCustomerCenter.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function MoreCustomerCenter() {
   return (
-    <div>MoreCustomerCenter</div>
+  <div style={{margin:'120px auto', width:'900px',height:'520px'}}>
+    {/* <img style={{width:'100%'}} src='../../../images/page_wait.jpg' alt='오버플로우 방지턱'/> */}
+    <img style={{width:'100%'}} src='../../../images/img_page_ready.png' alt='준비중이미지'/>
+  </div>
   )
 }
--- src/pages/Online.js
+++ src/pages/Online.js
@@ -3,8 +3,12 @@ import React from 'react'
 export default function Online() {
   return (
     <div>
-      <div style={{margin:'100px auto', width:'880px',height:'600px'}}>
+      {/* <div style={{margin:'100px auto', width:'880px',height:'600px'}}>
         <img style={{width:'100%'}} src='../../../images/page_wait.jpg' alt='오버플로우 방지턱'/>
+      </div> */}
+      <div style={{margin:'120px auto', width:'900px',height:'520px'}}>
+        {/* <img style={{width:'100%'}} src='../../../images/page_wait.jpg' alt='오버플로우 방지턱'/> */}
+        <img style={{width:'100%'}} src='../../../images/img_page_ready.png' alt='준비중이미지'/>
       </div>
     </div>
   )
